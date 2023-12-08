c9a8324092c4d8e1828ac161c168c7435e125028
RayRemnant <rayremnant.me@gmail.com>
2023-12-08 18:35:25 +0000

    trying something else

--- api/run.js
+++ api/run.js
@@ -1,28 +1,21 @@
-// api/run.js
-import edgeChromium from 'chrome-aws-lambda'
+const chromium = require("@sparticuz/chromium");
+const puppeteer = require("puppeteer-core");
 
-// Importing Puppeteer core as default otherwise
-// it won't function correctly with "launch()"
-import puppeteer from 'puppeteer-core'
-
-// You may want to change this if you're developing
-// on a platform different from macOS.
-// See https://github.com/vercel/og-image for a more resilient
-// system-agnostic options for Puppeteeer.
-const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-
-export default async function (req, res) {
-    // Edge executable will return an empty string locally.
-    const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
-
+module.exports = async (req, res) => {
     const browser = await puppeteer.launch({
-        executablePath,
-        args: edgeChromium.args,
-        headless: false,
-    })
+        args: chromium.args,
+        executablePath:
+            process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath),
+        headless: true,
+        // ...more config options
+    });
 
-    const page = await browser.newPage()
-    await page.goto('https://github.com')
+    const page = await browser.newPage();
+    //await page.goto(`<your-target-url>`, { waitUntil: "networkidle2" });
 
-    res.send('hello')
-}
+    // ...do something here
+
+    await browser.close();
+
+    res.send("hello");
+};
